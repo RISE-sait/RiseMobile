1e178055e96e11f65ccd800010d8c0782791c270
Mostapha45 <153405663+Mostapha45@users.noreply.github.com>
2025-01-15 18:26:16 -0700

    Began designing login ui and added images as well as converted them to usable code

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, TextInput, Image } from 'react-native'
 import { useAuth } from '../utils/auth';
 import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
+
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -17,59 +20,48 @@ const LoginScreen = () => {
   };
 
   return (
-    <SafeAreaView className="bg-white h-full">
-      <ScrollView contentContainerClassName="h-full">
-        {/* Login screen image placeholder */}
-        <View className="w-full h-3/6 bg-gray-300" />
+    <SafeAreaView className='bg-white h-full'>
+    <ScrollView contentContainerClassName='h-full'>
+        {/*Login screen image*/}
+        <Image source={images.onboarding} className='w-full h-3/6' resizeMode='contain' />
+        <View className='px-10'>
+            <Text className='text-base text-center uppercase font-rubik text-black-200'>
+                Welcome To RISE
+            </Text>
+            <Text className='text-3xl text-center uppercase font-rubik-bold text-black-300 mt-2'>
+                Where we help you RISE{"\n"}
+                <Text className='text-primary-300'>to the top</Text>
+            </Text>
+            <View>
+                <TextInput
+                    placeholder='Enter your email'
+                    className='mt-5 border-b-2 border-gray-300 w-full'
+                />
+                <TextInput
+                    placeholder='Enter your password'
+                    className='mt-5 border-b-2 border-gray-300 w-full'
+                />
+            </View>
+            <Text className='text-lg font-rubik-light text-black-200 mt-12 text-center'>
+                Login with Others!
+            </Text>
 
-        <View className="px-10">
-          <Text className="text-base text-center uppercase font-rubik text-black-200">
-            Welcome To RISE
-          </Text>
-          <Text className="text-3xl text-center uppercase font-rubik-bold text-black-300 mt-2">
-            Where we help you RISE{"\n"}
-            <Text className="text-primary-300">to the top</Text>
-          </Text>
-
-          <View>
-            <TextInput
-              placeholder="Enter your email"
-              className="mt-5 border-b-2 border-gray-300 w-full"
-            />
-            <TextInput
-              placeholder="Enter your password"
-              className="mt-5 border-b-2 border-gray-300 w-full"
-            />
-          </View>
-
-          <Text className="text-lg font-rubik-light text-black-200 mt-12 text-center">
-            Login with Others!
-          </Text>
-
-          {/* Login buttons */}
-          <View className="flex flex-row items-center justify-center gap-12">
-            <TouchableOpacity
-              onPress={handleLogin}
-              className="bg-white shadow-md shadow-zinc-300 rounded-full w-16 h-16 items-center justify-center mt-5"
-            >
-              <View className="w-5 h-5 bg-gray-300 rounded-full" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={handleLogin}
-              className="bg-white shadow-md shadow-zinc-300 rounded-full w-16 h-16 items-center justify-center mt-5"
-            >
-              <View className="w-5 h-5 bg-gray-300 rounded-full" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={handleLogin}
-              className="bg-white shadow-md shadow-zinc-300 rounded-full w-16 h-16 items-center justify-center mt-5"
-            >
-              <View className="w-5 h-5 bg-gray-300 rounded-full" />
-            </TouchableOpacity>
-          </View>
+            {/*Login buttons*/}
+            <View className='flex flex-row items-center justify-center gap-12'>
+                <TouchableOpacity className="bg-white shadow-md shadow-zinc-300 rounded-full w-16 h-16 items-center justify-center mt-5">
+                    <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-white shadow-md shadow-zinc-300 rounded-full w-16 h-16 items-center justify-center mt-5">
+                    <Image source={icons.facebook} className='w-5 h-5' resizeMode='contain' />
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-white shadow-md shadow-zinc-300 rounded-full w-16 h-16 items-center justify-center mt-5">
+                    <Image source={icons.apple} className='w-5 h-5' resizeMode='contain' />
+                </TouchableOpacity>
+            </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+    </ScrollView>
+</SafeAreaView>
+
   );
 };
 
---
+++ constants/icons.ts
@@ -0,0 +1,9 @@
+import google from '@/assets/icons/google.png';
+import facebook from "@/assets/icons/facebook.png";
+import apple from "@/assets/icons/apple.png";
+
+export default {
+    google,
+    facebook,
+    apple,
+};
