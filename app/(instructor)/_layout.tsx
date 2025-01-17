77f7596b8d6fc0c6bbfbaf9b4c31d6bac4d784eb
Mostapha45 <153405663+Mostapha45@users.noreply.github.com>
2025-01-16 21:57:29 -0700

    changed file names for instructor as well as updated tab navigation in layout file

--- app/(instructor)/_layout.tsx
+++ app/(instructor)/_layout.tsx
@@ -1,13 +1,101 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
+import { View, Image, Text } from "react-native";
+import icons from "@/constants/icons";
 
-export default function InstructorLayout() {
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => (
+  <View className="flex-1 mt-3 flex flex-col items-center">
+    <Image
+      source={icon}
+      style={{ tintColor: focused ? "#876A1D" : "#666876" }}
+      resizeMode="contain"
+      className="w-6 h-6"
+    />
+    <Text
+      className={`${
+        focused
+          ? "text-gold-100 font-rubik-medium"
+          : "text-black-200 font-rubik"
+      } text-xs w-full text-center mt-1`}
+    >
+      {title}
+    </Text>
+  </View>
+);
+
+const InstructorLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ title: 'Home' }} />
-      <Tabs.Screen name="matches" options={{ title: 'Matches' }} />
-      <Tabs.Screen name="calendar" options={{ title: 'Calendar' }} />
-      <Tabs.Screen name="book" options={{ title: 'Book' }} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "black",
+          position: "absolute",
+          borderTopColor: "transparent",
+          borderTopWidth: 1,
+          minHeight: 70,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="instructorHome"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.home} title="Home" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="instructorMatches"
+        options={{
+          title: "Matches",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.matches} title="Matches" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="instructorCalendar"
+        options={{
+          title: "Calendar",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.calendar} title="Calendar" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="instructorBook"
+        options={{
+          title: "Book",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.booking} title="Book" focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="instructorProfile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.person} title="Profile" focused={focused} />
+          ),
+        }}
+      />
     </Tabs>
   );
-}
+};
+
+export default InstructorLayout;
