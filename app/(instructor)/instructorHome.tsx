8687cbc0ee648aa390f771393baa19f97a9cb5c2
Mostapha Alahmair <153405663+Mostapha45@users.noreply.github.com>
2025-01-20 12:48:16 -0700

    implemented qr code component within instructor home screen.

--- app/(instructor)/instructorHome.tsx
+++ app/(instructor)/instructorHome.tsx
@@ -6,6 +6,8 @@ import { StatusBar } from "expo-status-bar";
 import GoToCards from "../components/GoToCards";
 import UpcomingCard from "../components/UpcomingCard";
 import SlideUpModal from "../components/SlideUpModal";
+import ProfileHeader from "../components/ProfileHeader";
+import QRCodeButton from "../components/QRCodeButton";
 
 export default function InstructorHomeScreen() {
   const handleNavigate = (route) => {
@@ -32,16 +34,7 @@ export default function InstructorHomeScreen() {
 
         {/* QR Code Button */}
         <View className="absolute top-8 left-10 z-50">
-          <TouchableOpacity
-            onPress={toggleModal}
-            className="w-12 h-12 bg-gray-900 rounded-full flex justify-center items-center shadow-md"
-          >
-            <Image
-              source={images.qrcode}
-              className="w-6 h-6"
-              style={{ resizeMode: "contain" }}
-            />
-          </TouchableOpacity>
+          <QRCodeButton onPress={toggleModal} />
         </View>
 
         {/* Modal */}
@@ -53,41 +46,15 @@ export default function InstructorHomeScreen() {
         </SlideUpModal>
 
         {/* Header Section */}
-        <View className="w-full px-10 mt-28">
-          <View className="bg-[#B59422] h-60 rounded-3xl overflow-hidden relative flex px-4 items-center">
-            <View className="absolute top-4 left-4 flex flex-row items-center">
-              <Image
-                source={images.schoolLogo} 
-                className="w-10 h-10 mt-2"
-                style={{ resizeMode: "contain" }}
-              />
-            </View>
-            {/* Number with Opacity (Placed Behind the Image) */}
-            <Text
-              className="text-white font-bold text-[170px] absolute right-4 bottom-2 opacity-25"
-              style={{ zIndex: 1 }}
-            >
-              34
-            </Text>
-
-            {/* Headshot Image */}
-            <Image
-              source={images.instructorHeadshot} 
-              className="absolute w-52 h-52 right-4 bottom-0"
-              style={{ resizeMode: "cover", borderRadius: 10, zIndex: 2 }}
-            />
-
-            {/* Name and Department Info */}
-            <View className="absolute bottom-4 left-4">
-              {/* First Name */}
-              <Text className="text-white-100 font-Oswald-SemiBold text-4xl uppercase">Sarah</Text>
-              
-              {/* Last Name */}
-              <Text className="text-white-100 font-Oswald-SemiBold text-4xl uppercase">Smith</Text>
-              {/* Department */}
-              <Text className="text-black-100 font-Oswald-Bold text-base mb-1">E-Commerce</Text>
-            </View>
-          </View>
+        <View className="w-full px-6 mt-20">
+          <ProfileHeader
+        firstName="Sam"
+        lastName="Smith"
+        role="Instructor - Basketball"
+        number="34"
+        profileImage={images.instructorHeadshot}
+        logo={images.schoolLogo}
+      />
         </View>
 
         {/* Upcoming Card Section */}
