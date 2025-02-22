27a71346760f727d678faf9b8616f70ffa9ba422
Mostapha45 <153405663+Mostapha45@users.noreply.github.com>
2025-02-21 17:55:58 -0700

    updated layouts to reflect changes needed such as removing tab bar for seperate screens and implementing routes for multiple screens.

---
+++ app/(athlete)/screens/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function ScreensLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="events" options={{ presentation: "modal" }} />
+      <Stack.Screen name="membership" options={{ presentation: "modal" }} />
+      <Stack.Screen name="store" options={{ presentation: "modal" }} />
+      <Stack.Screen name="store/product-details/[id]" options={{ presentation: "modal" }} />
+      <Stack.Screen name="store/checkout" options={{ presentation: "modal" }} />
+      <Stack.Screen name="event-details/[id]" options={{ presentation: "fullScreenModal" }} />
+      <Stack.Screen name="store/cart" options={{ presentation: "modal" }} />
+          </Stack>
+  );
+}
