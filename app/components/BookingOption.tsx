db5fc2f9f8095e15d0fb259b2959eea8d106949f
Mostapha45 <153405663+Mostapha45@users.noreply.github.com>
2025-02-23 23:39:33 -0700

    Created components for booking options to be reused for multiple roles.

---
+++ app/components/BookingOption.tsx
@@ -0,0 +1,77 @@
+import React, { useRef } from "react";
+import { Animated, TouchableOpacity, View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { Platform } from "react-native";
+
+interface BookingOptionButtonProps {
+  title: string;
+  icon: keyof typeof FontAwesome6.glyphMap;
+  wide?: boolean;
+  onPress?: () => void;
+}
+
+const BookingOptionButton: React.FC<BookingOptionButtonProps> = ({
+  title,
+  icon,
+  wide = false,
+  onPress = () => {},
+}) => {
+  const scaleAnim = useRef(new Animated.Value(1)).current;
+  const opacityAnim = useRef(new Animated.Value(1)).current;
+
+  const handlePressIn = () => {
+    Animated.parallel([
+      Animated.spring(scaleAnim, { toValue: 0.93, friction: 5, useNativeDriver: true }),
+      Animated.timing(opacityAnim, { toValue: 0.85, duration: 150, useNativeDriver: true }),
+    ]).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.parallel([
+      Animated.spring(scaleAnim, { toValue: 1, friction: 5, useNativeDriver: true }),
+      Animated.timing(opacityAnim, { toValue: 1, duration: 150, useNativeDriver: true }),
+    ]).start();
+  };
+
+  return (
+    <Animated.View
+      style={{
+        transform: [{ scale: scaleAnim }],
+        opacity: opacityAnim,
+      }}
+      className={`${wide ? "w-full" : "w-full"} my-2 rounded-3xl shadow-2xl shadow-black overflow-hidden`}
+    >
+      <TouchableOpacity
+        activeOpacity={0.75}
+        onPress={onPress}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+      >
+        <LinearGradient
+          colors={["rgba(255,255,255,0.2)", "rgba(255,255,255,0.05)"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          className="rounded-2xl px-4 items-center border border-white-100/20 backdrop-blur-lg overflow-hidden"
+          style={{ paddingLeft: Platform.OS === "ios" ? 5 : 0}}
+        >
+          <View 
+            className="bg-gold-100 w-16 h-16 rounded-full items-center justify-center mb-4 shadow-lg shadow-black/50"
+            style={{ marginTop: 10 }}>
+            <FontAwesome6 name={icon} size={28} color="#0C0B0B" />
+          </View>
+          <Text
+            className={`text-white-100 text-xl font-extrabold uppercase tracking-[2px] ${
+              Platform.OS === "ios" ? "ml-2 mb-2" : "text-center"
+            }`}
+          >
+            {title}
+          </Text>
+
+        </LinearGradient>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+export default BookingOptionButton;
