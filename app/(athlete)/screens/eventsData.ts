9fa46fdf2c2ec4fad0e7310004c903d3fc8dc64f
Mostapha45 <153405663+Mostapha45@users.noreply.github.com>
2025-02-23 23:42:17 -0700

    Mock data to be used in place of api for now for matches and events to build the UI

--- app/(athlete)/screens/eventsData.ts
+++ app/(athlete)/screens/eventsData.ts
@@ -6,6 +6,7 @@ export interface Event {
     location: string;
     image: string;
     description: string;
+    type: "event";
   }
   
   // Mock API Data
@@ -18,24 +19,27 @@ export interface Event {
       location: "Main Court",
       image: "https://images.unsplash.com/photo-1602105155315-077e218d8654?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       description: "Join our exclusive training camp to sharpen your basketball skills with professional coaches.",
+      type: "event",
     },
     {
       id: "2",
       title: "Speed & Agility Workshop",
-      date: "2025-03-20",
+      date: "2025-02-20",
       time: "2:00 PM",
       location: "Training Field",
       image: "https://plus.unsplash.com/premium_photo-1685366454607-0faf2157945b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       description: "Improve your speed and agility with specialized drills from top trainers.",
+      type: "event",
     },
     {
       id: "3",
       title: "Nutrition for Athletes Seminar",
-      date: "2025-03-25",
+      date: "2025-02-25",
       time: "1:00 PM",
       location: "Conference Room",
       image: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       description: "Learn about proper nutrition and meal plans for peak athletic performance.",
+      type: "event",
     },
   ];
   
